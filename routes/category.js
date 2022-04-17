054eb28b8443ce731444be19c83278547daf578a
Aleksandr <oleksandr.malishevskyi@gmail.com>
2022-04-17 12:22:35 +0200

    first commit

---
+++ routes/category.js
@@ -0,0 +1,14 @@
+const express = require('express')
+/* const passport = require('passport')
+const upload = require('../middleware/upload') */
+const controller = require('../controllers/category')
+const router = express.Router()
+
+router.get('/', controller.getAll)
+router.get('/:id', controller.getById)
+router.delete('/:id', controller.remove)
+router.post('/', controller.create)
+router.patch('/:id', controller.update)
+
+
+module.exports = router
