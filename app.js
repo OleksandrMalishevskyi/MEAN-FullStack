b0fbf8683be4ada0a33384c25d42d1afccd005e5
Aleksandr <oleksandr.malishevskyi@gmail.com>
2022-04-17 13:52:40 +0200

    add models

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 const app = express()
+
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(require('cors')())
+
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
